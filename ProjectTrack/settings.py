558ae2ecfb3bc78d3450ab6f49d2cb1f12739233
OSCAR-CASALS <138677821+OSCAR-CASALS@users.noreply.github.com>
2023-12-05 17:21:59 +0100

    Update settings.py

--- ProjectTrack/settings.py
+++ ProjectTrack/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t@@g9y*w+1^g33j%$6g9)x(v6d7!0rzci!^ql-y6dg1ntiq@ty'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
